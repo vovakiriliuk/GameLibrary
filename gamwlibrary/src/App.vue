e600284db9cdb0ed821853be98568b23c76c7810
vovakiriliuk <163052407+vovakiriliuk@users.noreply.github.com>
2025-11-09 13:55:15 +0200

    test header

--- gamwlibrary/src/App.vue
+++ gamwlibrary/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Button from './components/Button.vue';
+import Header from './components/Header.vue';
 import TestComponent from './components/TestComponent.vue';
 
 </script>
 
 <template>
+  <Header/>
   <TestComponent />
   <Button />
 </template>
---
+++ gamwlibrary/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="" alt="logoImg" class="logo">
+            <h3>BestGame</h3>
+        </div>
+
+        <nav>
+            <ul>
+                <li>Games</li>
+                <li>Hits</li>
+                <li>For gamers</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+    header{
+        background: var(--additional-color);
+        position: sticky;
+        top: 0;
+        left: 0;
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 16px;
+    }
+
+    .logo{
+        display: flex;
+    }
+
+    nav {
+        ul {
+            list-style-type: none;
+            display: flex;
+
+            li {
+                padding: 5px;
+                color: var(--main-text-color);
+            }
+        }
+
+    }
+</style>
